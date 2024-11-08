6842f0769e44876af1836c384061281ddf2819cd
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2024-11-08 16:21:04 -0300

    fix: change on click function for logout button

--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,22 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { IoMdAdd } from "react-icons/io";
-import { userLogoutThunk } from "../../store/modules/user/thunks";
+import { getUserThunk, userLogoutThunk } from "../../store/modules/user/thunks";
 import { TechList } from "../../components/TechList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AddTechModal } from "../../components/TechList/AddTechModal";
 import { EditTechModal } from "../../components/TechList/EditTechModal";
+import { useNavigate } from "react-router-dom";
 
 export function DashboardPage() {
   const { user, techs } = useSelector((state) => state);
+
   const dispatch = useDispatch();
   const [addTechOpen, setAddTechOpen] = useState(false);
   const [editTech, setEditTech] = useState(null);
+  const navigate = useNavigate();
+
+
   return (
     <>
       <header>
         <div>
           <img src="" alt="" />
-          <button onClick={dispatch(userLogoutThunk)}>Sair</button>
+          <button onClick={() => dispatch(userLogoutThunk(navigate))}>
+            Sair
+          </button>
         </div>
       </header>
       <main>
@@ -34,11 +41,17 @@ export function DashboardPage() {
                 <IoMdAdd size={20} />
               </button>
             </div>
-            <div>{techs?.length > 0 ? <TechList setEditTech={setEditTech}/> : null}</div>
+            <div>
+              {techs?.length > 0 ? (
+                <TechList setEditTech={setEditTech} />
+              ) : null}
+            </div>
           </div>
         </section>
         {addTechOpen ? <AddTechModal setAddTechOpen={setAddTechOpen} /> : null}
-        {editTech ? <EditTechModal editTech={editTech} setEditTech={setEditTech}/> : null}
+        {editTech ? (
+          <EditTechModal editTech={editTech} setEditTech={setEditTech} />
+        ) : null}
       </main>
     </>
   );
